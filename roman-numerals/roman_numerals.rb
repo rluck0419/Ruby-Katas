37e17c86ae6592675a9867e599cad7c220d75a9b
Robert Luckfield <rluck0419@gmail.com>
2016-09-12 13:24:28 -0500

    flipped hash/key pairs for more-recent ruby hash style

--- roman-numerals/roman_numerals.rb
+++ roman-numerals/roman_numerals.rb
@@ -1,26 +1,26 @@
 class Integer
   ROMAN_NUMBERS = {
-    1000 => "M",
-     900 => "CM",
-     500 => "D",
-     400 => "CD",
-     100 => "C",
-      90 => "XC",
-      50 => "L",
-      40 => "XL",
-      10 => "X",
-       9 => "IX",
-       5 => "V",
-       4 => "IV",
-       1 => "I",
+    "M": 1000,
+    "CM": 900,
+    "D": 500,
+    "CD": 400,
+    "C": 100,
+    "XC": 90,
+    "L": 50,
+    "XL": 40,
+    "X": 10,
+    "IX": 9,
+    "V": 5,
+    "IV": 4,
+    "I": 1
   }
 
   def to_roman
     number = self
     roman_numeral = ""
 
-    ROMAN_NUMBERS.each do |num, roman|
-      roman_numeral << roman * (number / num)
+    ROMAN_NUMBERS.each do |roman, num|
+      roman_numeral << roman.to_s * (number / num)
       number = number % num
     end
 
