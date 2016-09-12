d434e7b63183f9578f3842c17ab70c6663dffe62
Robert Luckfield <rluck0419@gmail.com>
2016-09-12 13:28:01 -0500

    completed robot-name

--- robot-name/robot_name.rb
+++ robot-name/robot_name.rb
@@ -1,5 +1,42 @@
 class Robot
-  def self.new
-    self.name = "AA000"
+  @names = []
+
+  attr_reader :name
+
+  def self.add_name(name)
+    @names << name
   end
+
+  def self.valid_name?(name)
+    !@names.include?(name)
+  end
+
+  def initialize
+    set_name
+  end
+
+  def reset
+    set_name
+  end
+
+  private
+
+  def set_name
+    @name = generate_name
+  end
+
+  def generate_name
+    loop do
+      name_candidate = ([*'A'..'Z'].shuffle.take(2) + [*1..9].shuffle.take(3)).join
+
+      if self.class.valid_name?(name_candidate)
+        self.class.add_name(name_candidate)
+        break name_candidate
+      end
+    end
+  end
+end
+
+module BookKeeping
+  VERSION = 2
 end
