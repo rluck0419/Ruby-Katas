782a87f803bdd4d1fe629f65835360c8a5440eaf
Robert Luckfield <rluck0419@gmail.com>
2016-09-13 14:48:12 -0500

    refactor 1

--- word-count/word_count.rb
+++ word-count/word_count.rb
@@ -1,15 +1,17 @@
 class Phrase
   def initialize(phrase)
-    @words = phrase.downcase.gsub(/[^a-zA-Z\d']/, " ").split(" ")
+    @words = phrase.downcase.gsub(/[^a-zA-Z\d']/, " ").split(" ").map do |w|
+      if w[0] == "'" && w[-1] == "'"
+        w = w.gsub("'", "")
+      else
+        w
+      end
+    end
   end
 
   def word_count
     word_count = {}
     @words.map do |w|
-      if w[0] == "'" && w[-1] == "'"
-        w = w[1..-2]
-      end
-
       if word_count[w]
         word_count[w] += 1
       else
