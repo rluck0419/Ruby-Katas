257af5bea1f38fb3f9b3ff8e01825d061f510eaa
Robert Luckfield <rluck0419@gmail.com>
2016-09-13 15:17:40 -0500

    completed bob kata

--- bob/bob.rb
+++ bob/bob.rb
@@ -1,13 +1,14 @@
 class Bob
   def hey(remark)
-    if remark.include?("?")
-      if remark.match(/[a-z]/)
+    return 'Fine. Be that way!' if remark.strip.length == 0
+    if remark[-1] == "?"
+      if remark.match(/[a-z]|\d/)
         'Sure.'
       else
         'Whoa, chill out!'
       end
     else
-      if remark.match(/[a-z]/)
+      if remark.match(/[a-z]/) || remark.match(/\d/) && !remark.match(/[A-Z]/)
         'Whatever.'
       else
         'Whoa, chill out!'
--- bob/bob_test.rb
+++ bob/bob_test.rb
@@ -63,61 +63,51 @@ class BobTest < Minitest::Test
   end
 
   def test_only_numbers
-    skip
     remark = '1, 2, 3'
     assert_equal 'Whatever.', bob.hey(remark), feedback(remark)
   end
 
   def test_question_with_only_numbers
-    skip
     remark = '4?'
     assert_equal 'Sure.', bob.hey(remark), feedback(remark)
   end
 
   def test_shouting_with_special_characters
-    skip
     remark = 'ZOMG THE %^*@#$(*^ ZOMBIES ARE COMING!!11!!1!'
     assert_equal 'Whoa, chill out!', bob.hey(remark), feedback(remark)
   end
 
   def test_shouting_with_no_exclamation_mark
-    skip
     remark = 'I HATE YOU'
     assert_equal 'Whoa, chill out!', bob.hey(remark), feedback(remark)
   end
 
   def test_statement_containing_question_mark
-    skip
     remark = 'Ending with ? means a question.'
     assert_equal 'Whatever.', bob.hey(remark), feedback(remark)
   end
 
   def test_prattling_on
-    skip
     remark = 'Wait! Hang on. Are you going to be OK?'
     assert_equal 'Sure.', bob.hey(remark), feedback(remark)
   end
 
   def test_silence
-    skip
     remark = ''
     assert_equal 'Fine. Be that way!', bob.hey(remark), feedback(remark)
   end
 
   def test_prolonged_silence
-    skip
     remark = ' ' * rand(1..10)
     assert_equal 'Fine. Be that way!', bob.hey(remark), feedback(remark)
   end
 
   def test_alternate_silences
-    skip
     remark = "\t" * rand(1..10)
     assert_equal 'Fine. Be that way!', bob.hey(remark), feedback(remark)
   end
 
   def test_on_multiple_line_questions
-    skip
     remark = %(
 Does this cryogenic chamber make me look fat?
 no)
