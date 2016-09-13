3b720f3b2ffaf078345444eb4214ec6f14e5a892
Robert Luckfield <rluck0419@gmail.com>
2016-09-13 14:15:58 -0500

    completion of grains kata

--- grains/grains.rb
+++ grains/grains.rb
@@ -1,5 +1,15 @@
 class Grains
   def self.square(n)
-    n
+    2 ** (n - 1)
+  end
+
+  def self.total
+    remaining = 64
+    grains = 0
+    while remaining > 0
+      grains += self.square(remaining)
+      remaining -= 1
+    end
+    grains
   end
 end
