31037a42b0f136470178f146738a76017db1ff68
Robert Luckfield <rluck0419@gmail.com>
2016-09-13 14:40:34 -0500

    initial completion of word count kata - examining for refactor now

--- word-count/word_count.rb
+++ word-count/word_count.rb
@@ -1,2 +1,25 @@
 class Phrase
+  def initialize(phrase)
+    @words = phrase.downcase.gsub(/[^a-zA-Z\d']/, " ").split(" ")
+  end
+
+  def word_count
+    word_count = {}
+    @words.map do |w|
+      if w[0] == "'" && w[-1] == "'"
+        w = w[1..-2]
+      end
+
+      if word_count[w]
+        word_count[w] += 1
+      else
+        word_count[w] = 1
+      end
+    end
+    word_count
+  end
+end
+
+module BookKeeping
+  VERSION = 1
 end
