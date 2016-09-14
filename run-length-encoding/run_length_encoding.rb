b6fd89b5a04106538a04b87a1ea6ce5877bf729d
Robert Luckfield <rluck0419@gmail.com>
2016-09-13 21:23:01 -0500

    passing only one test
    checking out for the night - going to sleep on this

--- run-length-encoding/run_length_encoding.rb
+++ run-length-encoding/run_length_encoding.rb
@@ -1,5 +1,28 @@
 class RunLengthEncoding
   def self.encode(string)
-    '2A3B4C'
+    letters = string.split("")
+    output = ""
+    count = 0
+
+    until letters.empty?
+      current = letters.first
+      while letters.shift == current
+        count += 1
+      end
+      if count > 1
+        output += count.to_s + current
+      else
+        output += current
+      end
+      count = 1
+    end
+    output
+  end
+
+  def self.decode(string)
+    letters = string.split("")
+    output = ""
+
+    'AABBBCCCC'
   end
 end
