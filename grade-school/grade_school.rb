29a2ddc438cf8ced101718dcbe7a0f97b65a5c15
Robert Luckfield <rluck0419@gmail.com>
2016-09-20 12:30:01 -0500

    key-value pairs now stored properly - now to sort and complete additional tests

--- grade-school/grade_school.rb
+++ grade-school/grade_school.rb
@@ -18,9 +18,10 @@ class School
   def students_by_grade
     students = []
     return students if @students.empty?
-    student_hash = {}
+    student_hash = { grade: 0, students: []}
     @students.map do |key, value|
-      student_hash.merge!(grade: value.to_i, students: key)
+      student_hash.merge!(grade: value.to_i)
+      student_hash[:students] << key
     end
     students << student_hash
   end
