e5b94618db946fa8e7cdc782922b4a03062efd4e
Robert Luckfield <rluck0419@gmail.com>
2016-09-20 12:01:37 -0500

    students stored in hash with grade as their key - mapped to array for output

--- grade-school/grade_school.rb
+++ grade-school/grade_school.rb
@@ -1,5 +1,17 @@
 class School
+  def initialize
+    @students = {}
+  end
+
+  def add(student, grade)
+    @students.merge!(grade => student)
+  end
+
   def students(grade)
     students = []
+    @students.map do |key, value|
+      students << value if key.to_i == grade
+    end
+    students
   end
 end
