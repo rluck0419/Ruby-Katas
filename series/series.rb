4f28c1c2b0a9c98e18bdec1937b2f4558cab2ca4
Robert Luckfield <rluck0419@gmail.com>
2016-09-21 16:36:16 -0500

    refactored initialize to remove extra map from slices
    using collect (init) and inject (slices), extra steps have been removed

--- series/series.rb
+++ series/series.rb
@@ -1,16 +1,12 @@
 class Series
   def initialize(digits)
-    @digits = digits
+    @digits = digits.split('').collect! { |n| n.to_i }
   end
 
   def slices(length)
     raise ArgumentError if @digits.length < length
-    output = []
-    @digits.length.times do |i|
-      digits = @digits.slice(i...length+i).split("")
-      break if digits.length < length
-      output << digits.map { |d| d.to_i }
+    (0..@digits.length-length).inject([]) do |output, i|
+      output << @digits[i...i+length]
     end
-    output
   end
 end
