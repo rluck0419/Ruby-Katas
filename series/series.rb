c4fdc2cf73edb8fa92c5081548af01f72c822404
Robert Luckfield <rluck0419@gmail.com>
2016-09-21 16:10:57 -0500

    passing first test

--- series/series.rb
+++ series/series.rb
@@ -1,2 +1,13 @@
 class Series
+  def initialize(digits)
+    @digits = digits
+  end
+
+  def slices(length)
+    output = []
+    @digits.length.times do |i|
+      output << [@digits[i...i+length].to_i]
+    end
+    output
+  end
 end
