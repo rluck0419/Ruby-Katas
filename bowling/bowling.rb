fbf13dbfcf22995dc5ceba92ebaaca37c7ea7056
Robert Luckfield <rluck0419@gmail.com>
2016-09-30 12:50:43 -0500

    setup and ready for spare - needs to track the last two rolls instead of just the one @prev variable

--- bowling/bowling.rb
+++ bowling/bowling.rb
@@ -1,11 +1,13 @@
 class Game
   def initialize
     @score = 0
+    @prev = 0
     @bonus = 1
   end
 
   def roll(a)
     @score += a * @bonus
+    
     if a == 10 || @prev == 10
       @bonus = 2
       @prev = a
--- bowling/bowling_test.rb
+++ bowling/bowling_test.rb
@@ -57,7 +57,6 @@ class GameTest < Minitest::Test
   end
 
   def test_should_score_a_spare_that_is_not_on_the_last_frame
-    skip
     @game.roll(5)
     @game.roll(5)
     @game.roll(3)
