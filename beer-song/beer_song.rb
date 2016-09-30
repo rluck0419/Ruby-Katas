43df2e29f63ecf34c2d84620a47651e88404f810
Robert Luckfield <rluck0419@gmail.com>
2016-09-30 12:24:24 -0500

    working for tests currently - refactored for a few verses

--- beer-song/beer_song.rb
+++ beer-song/beer_song.rb
@@ -1,15 +1,27 @@
 class BeerSong
-  def verse(current)
-    s, s2 = ''
-    remaining = current - 1
-    s = 's' if current > 1
-    s2 = 's' if remaining != 1
-    one = 'one'
-    if remaining == 0
-      remaining = 'no more'
-      one = 'it'
-    end
-    "#{current} bottle#{s} of beer on the wall, #{current} bottle#{s} of beer.\n" \
-      "Take #{one} down and pass it around, #{remaining} bottle#{s2} of beer on the wall.\n"
-  end
+	def verse(num)
+		"#{bottles(num).sub(/./){|s| s.upcase} } of beer on the wall, #{bottles(num)} of beer.\n" +
+			"#{pass_it_around(num)}, #{bottles(num-1)} of beer on the wall.\n"
+	end
+
+	def verses(first, last)
+		(last..first).map{ |v| verse(v) }.reverse.join("\n")
+	end
+
+	def lyrics()
+		verses(99, 0)
+	end
+
+	private def pass_it_around(num)
+		if num == 0
+			"Go to the store and buy some more"
+		else
+			"Take #{num == 1 ? 'it' : 'one'} down and pass it around"
+		end
+	end
+
+	private def bottles(num)
+		num = 99 if num == -1
+		"#{num == 0 ? 'no more' : num} bottle#{num == 1 ? '' : 's'}"
+	end
 end
--- beer-song/beer_song_test.rb
+++ beer-song/beer_song_test.rb
@@ -30,7 +30,6 @@ class BeerSongTest < Minitest::Test
   end
 
   def test_verse_0
-    skip
     expected = "No more bottles of beer on the wall, no more bottles of beer.\n" \
       "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
     assert_equal expected, BeerSong.new.verse(0)
