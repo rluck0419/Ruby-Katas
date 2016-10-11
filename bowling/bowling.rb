681aa64eb244373bd6aa9d077e5fc651f36614a7
Robert Luckfield <rluck0419@gmail.com>
2016-10-11 13:50:26 -0500

    closer to full-game scoring - multiple spares are scored correctly, but not multiple strikes

--- bowling/bowling.rb
+++ bowling/bowling.rb
@@ -1,19 +1,24 @@
 class Game
   def initialize
     @score = 0
-    @prev = 0
-    @bonus = 1
+    @prev1 = 0
+    @prev2 = 0
+    @strike_bonus = 1
+    @spare_bonus = 0
   end
 
   def roll(a)
-    @score += a * @bonus
-    
-    if a == 10 || @prev == 10
-      @bonus = 2
-      @prev = a
+    @score += a * @strike_bonus + @spare_bonus
+
+    if a == 10 || @prev1 == 10
+      @strike_bonus = 2
+    elsif @prev1 + @prev2 == 10
+      @score += a
     else
-      @bonus = 1
+      @strike_bonuse = 1
     end
+    @prev2 = @prev1
+    @prev1 = a
   end
 
   def score
--- bowling/bowling_test.rb
+++ bowling/bowling_test.rb
@@ -67,7 +67,6 @@ class GameTest < Minitest::Test
   end
 
   def test_should_score_multiple_strikes_in_a_row
-    skip
     @game.roll(10)
     @game.roll(10)
     @game.roll(10)
@@ -79,7 +78,6 @@ class GameTest < Minitest::Test
   end
 
   def test_should_score_multiple_spares_in_a_row
-    skip
     @game.roll(5)
     @game.roll(5)
     @game.roll(3)
