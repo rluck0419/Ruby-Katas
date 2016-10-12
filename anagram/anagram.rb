1e01595e16dd698a53efe7ff4b502ae157b0b2f1
Robert Luckfield <rluck0419@gmail.com>
2016-10-12 15:54:46 -0500

    passing all tests

--- anagram/anagram.rb
+++ anagram/anagram.rb
@@ -1,13 +1,15 @@
 class Anagram
   def initialize(word)
-    @letters = word.chars.sort
+    @letters = word.chars
     @word = word
   end
 
   def match(words)
     result = []
+    @letters = @letters.map { |l| l.downcase }.sort
     words.map do |w|
-      if w.chars.sort == @letters && w != @word
+      letters = w.chars.map { |l| l.downcase }.sort
+      if letters == @letters && w.downcase != @word.downcase
         result << w
       end
     end
