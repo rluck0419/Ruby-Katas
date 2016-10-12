bc6050bc0fca47a08919d1f5d33e3ff45b5b5d48
Robert Luckfield <rluck0419@gmail.com>
2016-10-12 15:44:59 -0500

    checks letters and creates array of matching words as result (anagrams)

--- anagram/anagram.rb
+++ anagram/anagram.rb
@@ -1,9 +1,15 @@
 class Anagram
   def initialize(word)
-    @word = word
+    @letters = word.chars.sort
   end
 
   def match(words)
-    []
+    result = []
+    words.map do |w|
+      if w.chars.sort == @letters
+        result << w
+      end
+    end
+    result.compact
   end
 end
