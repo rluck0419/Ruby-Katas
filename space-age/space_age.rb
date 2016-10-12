33b4f480bd3b9624fd4a845a559917b46acbeebc
Robert Luckfield <rluck0419@gmail.com>
2016-10-12 11:00:26 -0500

    removed unnecessary freezes

--- space-age/space_age.rb
+++ space-age/space_age.rb
@@ -1,6 +1,6 @@
 class SpaceAge
   attr_reader :seconds
-    SECONDS_IN_EARTH_YEAR = (60 * 60 * 24 * 365.25).to_f.freeze
+    SECONDS_IN_EARTH_YEAR = (60 * 60 * 24 * 365.25).to_f
     ORBITAL_PERIODS = { mercury: 0.2408467,
                         venus:   0.61519726,
                         earth:   1,
@@ -8,10 +8,10 @@ class SpaceAge
                         jupiter: 11.862615,
                         saturn:  29.447498,
                         uranus:  84.016846,
-                        neptune: 164.79132 }.freeze
+                        neptune: 164.79132 }
 
   def initialize(seconds)
-    @seconds = seconds.to_f.freeze
+    @seconds = seconds.to_f
   end
 
   ORBITAL_PERIODS.each do |planet, orbital_period|
