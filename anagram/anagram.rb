dbc3db223e105e07ac3125c7d899283a2d39ca35
Robert Luckfield <rluck0419@gmail.com>
2016-10-13 00:25:11 -0500

    refactored downcase - looking to refactor it further

--- anagram/anagram.rb
+++ anagram/anagram.rb
@@ -1,14 +1,14 @@
 class Anagram
   def initialize(word)
-    @letters = word.chars.map { |l| l.downcase }.sort
     @word = word.downcase
+    @letters = @word.chars.map { |l| l }.sort
   end
 
   def match(words)
-    words.select { |w| anagram?(w.downcase, w.chars.map { |l| l.downcase }.sort) }
+    words.map{ |word| word }.select { |w| anagram?(w, w.chars.map { |l| l.downcase }.sort) }
   end
 
   def anagram?(word, letters)
-    word != @word && letters == @letters
+    word.downcase != @word && letters == @letters
   end
 end
