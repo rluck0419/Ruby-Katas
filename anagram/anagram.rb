e4540d15f8bfbc67582542b9d7517031e301e661
Robert Luckfield <rluck0419@gmail.com>
2016-10-13 00:17:24 -0500

    reorder for clarity
    downcase is called in more logical places. also reordered - check if words are identical and if letters match

--- anagram/anagram.rb
+++ anagram/anagram.rb
@@ -1,14 +1,14 @@
 class Anagram
   def initialize(word)
     @letters = word.chars.map { |l| l.downcase }.sort
-    @word = word
+    @word = word.downcase
   end
 
   def match(words)
-    words.select { |w| anagram?(w, w.chars.map { |l| l.downcase }.sort) }
+    words.select { |w| anagram?(w.downcase, w.chars.map { |l| l.downcase }.sort) }
   end
 
   def anagram?(word, letters)
-    letters == @letters && word.downcase != @word.downcase
+    word != @word && letters == @letters
   end
 end
